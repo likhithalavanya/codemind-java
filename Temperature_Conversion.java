8a53d07e2fec542d6311fd116a4276649e8abb6e
likhithalavanya <119840461+likhithalavanya@users.noreply.github.com>
2023-08-05 11:16:21 +0530

    import java.util.Scanner; public class tri{         public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int c;         double f;         c = sc.nextInt();         f=32+(c*1.8);         System.out.printf("%.2f", f);         sc.close();         } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class tri{
+        public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int c;
+        double f;
+        c = sc.nextInt();
+        f=32+(c*1.8);
+        System.out.printf("%.2f", f);
+        sc.close();
+        }
+}
