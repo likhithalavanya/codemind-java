2c9c0a074632a7b92d0bb546a8c72810751c19ce
likhithalavanya <119840461+likhithalavanya@users.noreply.github.com>
2023-10-13 11:17:15 +0530

    import java.util.Scanner; public class CanShareEqually{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         if(n>=0)         {             System.out.printf("Positive Number");         }         else         {             System.out.printf("Negative Number");         }     } }

---
+++ Program_to_check_whether_the_given_integer_is_positive_or_negative.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class CanShareEqually{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n>=0)
+        {
+            System.out.printf("Positive Number");
+        }
+        else
+        {
+            System.out.printf("Negative Number");
+        }
+    }
+}
